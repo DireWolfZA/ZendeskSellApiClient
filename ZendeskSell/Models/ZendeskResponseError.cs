a6565236080348e69944d0242805e1f4df396d9e
░▒▓█│【Walkman】│█▓▒░ <MatthewC@direwolf.co.za>
2021-07-15 12:50:20 +0200

    updated response Errors and Metadata to match zendesk API reference

--- ZendeskSell/Models/ZendeskResponseError.cs
+++ ZendeskSell/Models/ZendeskResponseError.cs
@@ -1,14 +1,24 @@
-using System;
 using Newtonsoft.Json;
 
 namespace ZendeskSell.Models {
     public class ZendeskResponseError {
         [JsonProperty("error")]
-        public string Error { get; set; }
-        [JsonProperty("error_description")]
-        public string Description { get; set; }
-        [JsonProperty("error_uri")]
-        public Uri Uri { get; set; }
+        public ZendeskResponseErrorData Error { get; set; }
+        [JsonProperty("meta")]
+        public ZendeskResponseMetadata Meta { get; set; }
+    }
+
+    public class ZendeskResponseErrorData {
+        [JsonProperty("resource")]
+        public string Resource { get; set; }
+        [JsonProperty("field")]
+        public string Field { get; set; }
+        [JsonProperty("code")]
+        public string Code { get; set; }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        [JsonProperty("details")]
+        public string Details { get; set; }
     }
 
 }
--- ZendeskSell/Models/ZendeskResponseMetadata.cs
+++ ZendeskSell/Models/ZendeskResponseMetadata.cs
@@ -2,17 +2,18 @@ using Newtonsoft.Json;
 
 namespace ZendeskSell.Models {
     public class ZendeskResponseMetadata {
-        [JsonProperty("count")]
-        public int Count { get; set; }
         [JsonProperty("type")]
         public string Type { get; set; }
+        [JsonProperty("http_status")]
+        public string HTTPStatus { get; set; }
+        [JsonProperty("logref")]
+        public string Logref { get; set; }
         [JsonProperty("links")]
         public ZendeskResponseLinkMetadata Links { get; set; }
-
     }
 
     public class ZendeskResponseLinkMetadata {
-        [JsonProperty("self")]
-        public string Self { get; set; }
+        [JsonProperty("more_info")]
+        public string MoreInfo { get; set; }
     }
 }
