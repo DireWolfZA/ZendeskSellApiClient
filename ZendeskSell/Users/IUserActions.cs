ff835383aa0e479a7a0446559bee7018a0e1d057
░▒▓█│【Walkman】│█▓▒░ <MatthewC@direwolf.co.za>
2021-08-12 09:51:46 +0200

    added more get users by parameters

--- ZendeskSell/Users/IUserActions.cs
+++ ZendeskSell/Users/IUserActions.cs
@@ -3,7 +3,7 @@ using ZendeskSell.Models;
 
 namespace ZendeskSell.Users {
     public interface IUserActions {
-        Task<ZendeskSellCollectionResponse<UserResponse>> GetAsync(int pageNumber, int numPerPage, string name = null);
+        Task<ZendeskSellCollectionResponse<UserResponse>> GetAsync(int pageNumber, int numPerPage, string name = null, string role = null, string status = null, bool? confirmed = null, string email = null);
         Task<ZendeskSellObjectResponse<UserResponse>> GetOneAsync(int id);
         Task<ZendeskSellObjectResponse<UserResponse>> GetCurrentAsync();
     }
--- ZendeskSell/Users/UserActions.cs
+++ ZendeskSell/Users/UserActions.cs
@@ -11,12 +11,21 @@ namespace ZendeskSell.Users {
             _client = client;
         }
 
-        public async Task<ZendeskSellCollectionResponse<UserResponse>> GetAsync(int pageNumber, int numPerPage, string name = null) {
+        public async Task<ZendeskSellCollectionResponse<UserResponse>> GetAsync(int pageNumber, int numPerPage, 
+                string name = null, string role = null, string status = null, bool? confirmed = null, string email = null) {
             var request = new RestRequest("users", Method.GET)
                               .AddParameter("page", pageNumber)
                               .AddParameter("per_page", numPerPage);
             if (name != null)
                 request.AddParameter("name", name);
+            if (role != null)
+                request.AddParameter("role", role);
+            if (status != null)
+                request.AddParameter("status", status);
+            if (confirmed != null)
+                request.AddParameter("confirmed", confirmed);
+            if (email != null)
+                request.AddParameter("email", email);
             return (await _client.ExecuteTaskAsync<ZendeskSellCollectionResponse<UserResponse>>(request, Method.GET)).Data;
         }
 
