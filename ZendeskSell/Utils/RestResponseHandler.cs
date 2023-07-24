f8a0dbfb1580671f3a1b66b5ce986e75042d3057
░▒▓█│【Walkman】│█▓▒░ <MatthewC@direwolf.co.za>
2023-07-24 12:37:21 +0200

    added handling HTML zendesk response with 504 Gateway Time-out

--- ZendeskSell/Utils/RestResponseHandler.cs
+++ ZendeskSell/Utils/RestResponseHandler.cs
@@ -13,10 +13,24 @@ namespace ZendeskSell.Utils {
                 throw ex;
             }
 
-            if (response.Content.StartsWith("{\"status\":")) {
+            if (response.Content.StartsWith("<html>")) { // usually <html><head><title>504 Gateway Time-out</title></head><body><center><h1>504 Gateway Time-out</h1></center><hr><center>nginx</center></body></html>
+                const string titleTag = "<title>";
+                const string h1Tag = "<h1>";
+                int titleStartPosition = response.Content.IndexOf(titleTag) + titleTag.Length;
+                int h1StartPosition = response.Content.IndexOf(h1Tag) + h1Tag.Length;
+
+                string title = response.Content.Substring(titleStartPosition, response.Content.IndexOf("</", titleStartPosition) - titleStartPosition);
+                string h1 = response.Content.Substring(h1StartPosition, response.Content.IndexOf("</", h1StartPosition) - h1StartPosition);
+
+                var ex = new System.Net.WebException($"Zendesk Sell Error:{title} Description:{h1}");
+                ex.Data[ExceptionDataKey] = response.Content;
+                throw ex;
+            }
+
+            if (response.Content.StartsWith(@"{""status"":")) { // usually {"status":500,"error":"Internal Server Error"}
                 var deserializer = new JsonDeserializer();
                 var statusWithError = deserializer.Deserialize<ZendeskResponseStatusWithError>(response);
-                if (statusWithError != null && statusWithError.Status != null) {
+                if (statusWithError != null && statusWithError.Status.HasValue) {
                     var ex = new System.Net.WebException($"Zendesk Sell Error: Status:{statusWithError.Status} Error:{statusWithError.Error}");
                     ex.Data[ExceptionDataKey] = response.Content;
                     throw ex;
